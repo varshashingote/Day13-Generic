1d71b453bd8fd8282056f67a04c5f68e69f66f72
varsha Gunjal <varshashingote01@gmail.com>
2023-02-08 12:56:39 +0530

    [varsha] max string among Three String

--- Day13GenericMaxNumber/MaxNoAmongThree.cs
+++ Day13GenericMaxNumber/MaxNoAmongThree.cs
@@ -42,6 +42,25 @@ namespace Day13GenericMaxNumber
             throw new Exception("firstValue ,SecondValue and thirdValue are same");
 
         }
+        public static String MaxSringNumberCheck(String firstValue, String secondValue,String thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue ,SecondValue and thirdValue are same");
+
+        }
+
+
 
     }
 }
--- Day13GenericMaxNumber/Program.cs
+++ Day13GenericMaxNumber/Program.cs
@@ -13,8 +13,10 @@ namespace Day13GenericMaxNumber
             MaxNoAmongThree maxNoAmongThree=new MaxNoAmongThree();
             int output= MaxNoAmongThree.MaxIntegerNumberCheck(10, 20, 30);
             float output2 = MaxNoAmongThree.MaxFloatNumberCheck(12.3f, 32.3f, 44.3f);
+            String output3 = MaxNoAmongThree.MaxSringNumberCheck("Varsha", "Suraj", "vasu");
             Console.WriteLine(output);
             Console.WriteLine(output2);
+            Console.WriteLine(output3);
             Console.ReadLine();
 
         }
