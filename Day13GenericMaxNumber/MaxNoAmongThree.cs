1940f8759d6670993bc4ce6ba7b2ea86501bfbc7
varsha Gunjal <varshashingote01@gmail.com>
2023-02-08 12:24:39 +0530

    [varsha]Maximum Number among Three Interger

---
+++ Day13GenericMaxNumber/MaxNoAmongThree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13GenericMaxNumber
+{
+    public  class MaxNoAmongThree
+    {
+        public static int  MaxIntegerNumberCheck(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue ,SecondValue and thirdValue are same");
+            
+        }
+    }
+}
+
---
+++ Day13GenericMaxNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13GenericMaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxNoAmongThree maxNoAmongThree=new MaxNoAmongThree();
+            int output= MaxNoAmongThree.MaxIntegerNumberCheck(10, 80, 30);
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}
